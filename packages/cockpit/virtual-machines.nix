14c019163103fb2989cc38725ff4ba43c3625a1e
Christopher Fair <chris.fair@novatechautomation.com>
2025-05-14 13:12:05 -0500

    Updated to 331 for cockpit-machines
    Signed-off-by: Christopher Fair <chris.fair@novatechautomation.com>

--- packages/cockpit/virtual-machines.nix
+++ packages/cockpit/virtual-machines.nix
@@ -1,45 +1,27 @@
-{ lib, stdenv, fetchzip, gettext, git, nodejs_18, python3 }:
+{ lib, stdenv, fetchzip, gettext, git, nodejs_18, python3, pkg-config }:
 
 stdenv.mkDerivation rec {
   pname = "cockpit-machines";
   version = "331";
 
   src = fetchzip {
-    url = "https://github.com/cockpit-project/cockpit-machines/releases/download/${version}/cockpit-machines-${version}.tar.xz";
-    sha256 = "x16eynAUoOqAw4FbbXus3+jus/HEnxFfXvyHkki5d2A=";
+    url = "https://github.com/cockpit-project/cockpit-machines/archive/refs/tags/${version}.tar.gz";
+    sha256 = "0r52wl4ap4jwxy4p8j0pvs4r9jww1dp2n84j0y5vxl2wvz1nm0b6"; # Update via `nix-prefetch-url`
   };
 
   nativeBuildInputs = [
-    git
-    nodejs_18
     gettext
+    nodejs_18
     python3
+    pkg-config
+    git
   ];
 
-  makeFlags = [ "PREFIX=" "DESTDIR=$(out)" ];
-
-  postPatch = ''
-    substituteInPlace Makefile --replace /usr/share $out/share
-    touch pkg/lib/cockpit.js
-    touch pkg/lib/cockpit-po-plugin.js
-    cat > dist/manifest.json <<EOF
-{
-  "module": "machines",
-  "index": "index.html",
-  "requires": ["cockpit", "libvirt"],
-  "js": "index.js",
-  "css": "index.css",
-  "menu": {
-    "vms": {
-      "label": "Virtual Machines",
-      "path": "index.html",
-      "order": 60,
-      "keywords": ["libvirt", "vm", "kvm", "qemu", "virtual"]
-    }
-  }
-}
-EOF
-'';
+  buildPhase = ''
+    runHook preBuild
+    make
+    runHook postBuild
+  '';
 
   installPhase = ''
     runHook preInstall
@@ -47,16 +29,6 @@ EOF
     cp -r dist/* $out/share/cockpit/machines
     runHook postInstall
   '';
-
-  postFixup = ''
-    gunzip "$out/share/cockpit/machines/index.js.gz"
-
-    sed -i "s#/usr/bin/python3#/usr/bin/env python3#g" "$out/share/cockpit/machines/index.js"
-    sed -i "s#/usr/bin/pwscore#/usr/bin/env pwscore#g" "$out/share/cockpit/machines/index.js"
-    gzip -9 "$out/share/cockpit/machines/index.js"
-  '';
-
-  dontBuild = true;
 
   meta = with lib; {
     description = "Cockpit UI for virtual machines";
